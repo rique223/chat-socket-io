045da58d074a8f1cd4597377a5b9b45935f981de
rique223 <henrique.jobs1@gmail.com>
2022-06-22 12:19:27 -0300

    Yarn use and new greetings and farewell messages

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-let app = require('express')();
-let http = require('http').createServer(app);
-let io = require('socket.io')(http);
+let app = require("express")();
+let http = require("http").createServer(app);
+let io = require("socket.io")(http);
 
 const users = {};
 
-app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', (socket) => {
-	socket.on('chat message', (msg) => {
-		io.emit('chat message', { message: msg, name: users[socket.id] });
-	});
+io.on("connection", (socket) => {
+    socket.on("chat message", (msg) => {
+        io.emit("chat message", { message: msg, name: users[socket.id] });
+    });
 
-	socket.on('new-user', (name) => {
-		users[socket.id] = name;
-	});
+    socket.on("new-user", (name) => {
+        users[socket.id] = name;
+    });
 
-	socket.on('greeting', () => {
-		io.emit('greeting');
-	});
+    socket.on("greeting", () => {
+        io.emit("greeting", { user: users[socket.id] });
+    });
 
-	socket.on('disconnect', () => {
-		io.emit('farewell');
-	});
+    socket.on("disconnect", () => {
+        io.emit("farewell", { user: users[socket.id] });
+    });
 });
 
 http.listen(3000, () => {
-	console.log('listening on http://localhost:3000');
+    console.log("listening on http://localhost:3000");
 });
